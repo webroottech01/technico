d7c6eb031bb11d49e0455cc16436018f29a84f74
Alok Anand <alokmonu289@gmail.com>
2025-03-19 15:05:49 +0530

    aaded adminpanel

--- backend/database/migrations/2025_03_06_132018_create_contacts_table.php
+++ backend/database/migrations/2025_03_06_132018_create_contacts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email');
+            $table->string('company_name');
             $table->text('message');
             $table->timestamps();
         });
