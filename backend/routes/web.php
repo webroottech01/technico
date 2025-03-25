e9c1c63de50a7d353654d5c2c6462d296dae75c1
Khushal024 <khushalranka@gmail.com>
2025-03-25 16:39:11 +0530

    pushing

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ContactController;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Response;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,14 +14,9 @@ use Illuminate\Support\Facades\Response;
 |
 */
 
-
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
 
-Route::get('/frontend/build/{path}', function ($path) {
-    $file = public_path("frontend/build/{$path}");
-    if (File::exists($file)) {
-        return Response::file($file);
-    }
-    abort(404);
-})->where('path', '.*');
